2f791a86221af62779001332991a991307b07554
tokudu <antonlopyrev@gmail.com>
2013-05-31 11:35:57 -0700

    Initial Commit

---
+++ src/com/sothree/slidinguppaneldemo/DemoActivity.java
@@ -0,0 +1,61 @@
+package com.sothree.slidinguppaneldemo;
+
+import com.sothree.slidinguppaneldemo.SlidingUpPanelLayout.PanelSlideListener;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.text.method.LinkMovementMethod;
+import android.view.Menu;
+import android.view.View;
+import android.view.Window;
+import android.widget.TextView;
+
+public class DemoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR_OVERLAY);
+        setContentView(R.layout.activity_demo);
+
+        SlidingUpPanelLayout layout = (SlidingUpPanelLayout) findViewById(R.id.sliding_layout);
+        layout.setShadowDrawable(getResources().getDrawable(R.drawable.above_shadow));
+        layout.setPanelSlideListener(new PanelSlideListener() {
+
+            @Override
+            public void onPanelSlide(View panel, float slideOffset) {
+                if (slideOffset < 0.2) {
+                    if (getActionBar().isShowing()) {
+                        getActionBar().hide();
+                    }
+                } else {
+                    if (!getActionBar().isShowing()) {
+                        getActionBar().show();
+                    }
+                }
+            }
+
+            @Override
+            public void onPanelExpanded(View panel) {
+
+
+            }
+
+            @Override
+            public void onPanelCollapsed(View panel) {
+
+
+            }
+        });
+        TextView t = (TextView) findViewById(R.id.brought_by);
+        t.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.demo, menu);
+        return true;
+    }
+
+}
