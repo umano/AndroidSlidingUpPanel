cf95b2af29c785e57e19871ba4ea9ad19a51fcf7
Jung Chang Hwan <shakejj@gmail.com>
2013-11-08 10:08:03 +0900

    Reset to original demo application DemoActivity

--- demo/src/com/sothree/slidinguppanel/demo/DemoActivity.java
+++ demo/src/com/sothree/slidinguppanel/demo/DemoActivity.java
@@ -20,7 +20,6 @@ public class DemoActivity extends Activity {
         setContentView(R.layout.activity_demo);
 
         SlidingUpPanelLayout layout = (SlidingUpPanelLayout) findViewById(R.id.sliding_layout);
-        layout.setIsTransparent(true);
         layout.setShadowDrawable(getResources().getDrawable(R.drawable.above_shadow));
         layout.setAnchorPoint(0.3f);
         layout.setPanelSlideListener(new PanelSlideListener() {
