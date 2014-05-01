62f45828f9ade33937ada55c8d6701e1656b2942
Ken Jones <kjones@peaksware.com>
2014-05-01 13:56:03 -0600

    Set slide state before calling onPanelXxx event callbacks. Calls to isAnchored() and isExpanded() should return the correct value if called during the onPanelXxx event callbacks.

--- library/src/com/sothree/slidinguppanel/SlidingUpPanelLayout.java
+++ library/src/com/sothree/slidinguppanel/SlidingUpPanelLayout.java
@@ -1111,18 +1111,18 @@ public class SlidingUpPanelLayout extends ViewGroup {
                 if (mSlideOffset == 0) {
                     if (mSlideState != SlideState.EXPANDED) {
                         updateObscuredViewVisibility();
-                        dispatchOnPanelExpanded(mSlideableView);
                         mSlideState = SlideState.EXPANDED;
+                        dispatchOnPanelExpanded(mSlideableView);
                     }
                 } else if (mSlideOffset == (float)anchoredTop/(float)mSlideRange) {
                     if (mSlideState != SlideState.ANCHORED) {
                         updateObscuredViewVisibility();
-                        dispatchOnPanelAnchored(mSlideableView);
                         mSlideState = SlideState.ANCHORED;
+                        dispatchOnPanelAnchored(mSlideableView);
                     }
                 } else if (mSlideState != SlideState.COLLAPSED) {
-                    dispatchOnPanelCollapsed(mSlideableView);
                     mSlideState = SlideState.COLLAPSED;
+                    dispatchOnPanelCollapsed(mSlideableView);
                 }
             }
         }
