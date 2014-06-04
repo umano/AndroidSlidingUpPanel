a0c43941133c5788b6fe1b4ca6efadfd5e37e4ed
tokudu <antonlopyrev@gmail.com>
2014-06-04 16:23:21 -0700

    Fixed the top gravity support and dimming

--- library/src/com/sothree/slidinguppanel/SlidingUpPanelLayout.java
+++ library/src/com/sothree/slidinguppanel/SlidingUpPanelLayout.java
@@ -805,7 +805,7 @@ public class SlidingUpPanelLayout extends ViewGroup {
         // Compute the top of the panel if its collapsed
         return mIsSlidingUp
                 ? getMeasuredHeight() - getPaddingBottom() - mPanelHeight - slidePixelOffset
-                : getPaddingTop() + slidePixelOffset;
+                : getPaddingTop() - panelHeight + mPanelHeight + slidePixelOffset;
     }
 
     /*
@@ -957,7 +957,7 @@ public class SlidingUpPanelLayout extends ViewGroup {
         result = super.drawChild(canvas, child, drawingTime);
         canvas.restoreToCount(save);
 
-        if (drawScrim && mCoveredFadeColor > 0) {
+        if (drawScrim && mCoveredFadeColor != 0) {
             final int baseAlpha = (mCoveredFadeColor & 0xff000000) >>> 24;
             final int imag = (int) (baseAlpha * mSlideOffset);
             final int color = imag << 24 | (mCoveredFadeColor & 0xffffff);
