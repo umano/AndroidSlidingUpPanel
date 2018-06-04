f9c7cf3d81bc58d989721a93d99a3033d18e956c
Keifer Stone <keifer.stone@bottlerocketstudios.com>
2018-06-04 13:43:28 -0600

    Issue 749 - only abort drag helper if in idle state

--- library/src/main/java/com/sothree/slidinguppanel/SlidingUpPanelLayout.java
+++ library/src/main/java/com/sothree/slidinguppanel/SlidingUpPanelLayout.java
@@ -959,7 +959,9 @@ public class SlidingUpPanelLayout extends ViewGroup {
         final int action = MotionEventCompat.getActionMasked(ev);
 
         if (!isEnabled() || !isTouchEnabled() || (mIsUnableToDrag && action != MotionEvent.ACTION_DOWN)) {
-            mDragHelper.abort();
+            if (mDragHelper.getViewDragState() == ViewDragHelper.STATE_IDLE) {
+                mDragHelper.abort();
+            }
             return super.dispatchTouchEvent(ev);
         }
 
